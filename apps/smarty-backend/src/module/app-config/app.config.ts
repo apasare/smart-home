4baa516c79ce8091d153b4f3a3d81c81d21c3b1e
Alexandru Pasare <alex.simplecoding@gmail.com>
2022-07-25 01:27:06 +0300

    refactor: change default port and host for backend app

--- apps/smarty-backend/src/module/app-config/app.config.ts
+++ apps/smarty-backend/src/module/app-config/app.config.ts
@@ -1,6 +1,6 @@
 import { AppConfig } from "./app-config.interface";
 
 export default (): AppConfig => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-  host: process.env.HOST || '127.0.0.1',
+  port: parseInt(process.env.PORT, 10) || 4000,
+  host: process.env.HOST || '0.0.0.0',
 });
